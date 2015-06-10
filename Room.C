806dce8319964ecd3ca624f076bfdf13af921046
MichaelBun <micha3008@gmail.com>
2015-06-11 00:54:07 +0300

    Room Class added

--- Room.C
+++ Room.C
@@ -6,4 +6,33 @@ using std::cout;
 using std::endl;
 using std::boolalpha;
 
-// Add your code here ...
+Room::Room(unsigned int room_num, unsigned int room_space) : room_num_(room_num), room_space_(room_space), occupied_(false) {}; /*Constructor*/
+
+unsigned int Room::get_room_num() const /*returns room number*/
+{
+	return room_num_;
+}
+
+bool Room::get_occupied() const /*returns if the room is occupied*/
+{
+	return occupied_;
+}
+
+void Room::is_occupied() /*sets the room as occupied*/
+{
+	occupied_ = true;
+	return;
+}
+
+void Room::not_occupied() /*sets the room as not occupied*/
+{
+	occupied_ = false;
+	return;
+}
+
+void Room::print_room() const /*prints the room as asked*/
+{
+	cout << "Number : " << room_num_ << endl;
+	cout << "Size : " << room_space_ << " square meters" << endl;
+	cout << "Is Occupied : " << std::boolalpha << occupied_ << endl;
+}
