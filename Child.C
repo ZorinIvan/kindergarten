c28a6a781e56ff0cefe030b2706a9d9952015aa8
MichaelBun <micha3008@gmail.com>
2015-06-11 00:01:02 +0300

    Part1 - Start

--- Child.C
+++ Child.C
@@ -4,6 +4,31 @@ using std::cout;
 using std::endl;
 using std::boolalpha;
 
-// Add your code here
+Child::Child(string name, int age, string phone_number) : Person(age, name), Prnts_phone_number_(phone_number), sick_(false) {}; /*Child constructor*/
 
-/*TESTING TESTING*/
+string Child::get_phone() const /*Returns the perants phone number*/
+{
+	return Prnts_phone_number_;
+}
+
+Result Child::set_sick() /*Change child's sick status*/
+{
+	if (sick_ == true) /*The child is already sick*/
+		return FAILURE;
+	else{ /*The child isn't sick*/
+		sick_ = true;
+		return SUCCESS;
+	}
+}
+
+void Child::set_phone(string phone_number) /*Updating the current phone number*/
+{
+	Prnts_phone_number_ = phone_number;
+}
+
+void Child::print_child() const
+{
+	print_person(); /*First we print the Person info*/
+	cout << "Parant's Phone : " << Prnts_phone_number_ << endl; /*Then all of the rest*/
+	cout << "Is sick : " << std::boolalpha << sick_ << endl;
+}
--- Person.C
+++ Person.C
@@ -4,4 +4,20 @@ using std::string;
 using std::cout;
 using std::endl;
 
-// Add your code here ...
+
+Person::Person(int age, string name) : age_(age), name_(name) {}; /*Constructor*/
+
+string Person::get_name() const /*Gives the person's name*/
+{
+	return name_;
+}
+int Person::get_age() const /*Gives the persons age*/
+{
+	return age_;
+}
+
+void Person::print_person() const
+{
+	cout << "Name : " << name_ << endl; /*Printing the info we need to print*/
+	cout << "Age : " << age_ << endl;
+}
